2a474009572eac2b746fa482f66ea7d5dcb07a5f
Pavel Sarlov <psarlov@asteasolutions.com>
2024-12-02 14:33:31 +0200

    add day 1

---
+++ 01/solution.js
@@ -0,0 +1,37 @@
+import { chain, isNil, sum } from "lodash-es";
+import path from "path";
+import { fileURLToPath } from "url";
+import { getLines, subArray } from "../lib/helpers.js";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const lines = getLines(`${dirname}/input.txt`);
+
+const [arr1, arr2] = chain(lines)
+  .reduce(
+    (prev, line) => {
+      const [x, y] = line.split(/\s+/).map(Number);
+      prev[0].push(x);
+      prev[1].push(y);
+      return prev;
+    },
+    [[], []],
+  )
+  .map((arr) => arr.sort())
+  .value();
+
+function part1() {
+  return sum(subArray(arr1, arr2, true));
+}
+
+function part2() {
+  const scores = arr2.reduce((prev, curr) => {
+    const val = prev.get(curr);
+    return prev.set(curr, isNil(val) ? 1 : val + 1);
+  }, new Map());
+
+  return arr1.reduce((prev, curr) => prev + (scores.get(curr) ?? 0) * curr, 0);
+}
+
+console.log("part1", part1());
+console.log("part2", part2());
