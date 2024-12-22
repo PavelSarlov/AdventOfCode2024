7d849f9b52beefe6066a8bd0acbccbb14518e109
Pavel Sarlov <sarlovpavel@gmail.com>
2024-12-22 19:05:15 +0200

    add day 21 optimal

--- 21/solution.js
+++ 21/solution.js
@@ -1,7 +1,8 @@
+import { MinHeap } from "@datastructures-js/heap";
 import { chain } from "lodash-es";
 import { getLines, sumArray } from "../lib/helpers.js";
 
-const codes = getLines("example.txt");
+const codes = getLines("input.txt").map((l) => l.trim());
 const keypadNum = ["789", "456", "123", [, "0", "A"]];
 const keypadDir = [[, "^", "A"], "<v>"];
 const actions = ["A", "^", ">", "v", "<"];
@@ -16,8 +17,6 @@ function getPos(key, keypad) {
 function apply(action, key, keypad) {
   let pos = getPos(key, keypad);
   switch (action) {
-    case "A":
-      return [key, key];
     case "v":
       pos = sumArray(pos, [1, 0]);
       break;
@@ -32,97 +31,35 @@ function apply(action, key, keypad) {
       break;
   }
 
-  return [keypad[pos[0]]?.[pos[1]], null];
+  return keypad[pos[0]]?.[pos[1]];
 }
 
-function bfs(code) {
-  const start = ["", 0, ...Array(3).fill("A")];
-  const q = [];
-  q.push(start);
+function bfsPaths(start, end, keypad) {
+  const q = new MinHeap(([, d]) => d.length);
+  q.push([start, ""]);
 
-  const seen = new Set();
+  const paths = new Set();
+  while (q.size()) {
+    const [curr, d] = q.pop();
 
-  while (q.length) {
-    let [out, d, ...pads] = q.shift();
-
-    if (out === code) {
-      return d;
-    }
-
-    if (
-      pads.some((pad) => !pad) ||
-      !code.startsWith(out) ||
-      seen.has([out, ...pads].toString())
-    ) {
+    if (!curr || d.length > ([...paths][0]?.length ?? Infinity)) {
       continue;
     }
 
-    seen.add([out, ...pads].toString());
-
-    actions.forEach((action) => {
-      let newPad = null;
-      let newOut = "";
-      const newPads = [...pads];
-
-      for (let i = 0; i < pads.length; i++) {
-        [newPad, action] = apply(
-          action,
-          pads[i],
-          i === pads.length - 1 ? keypadNum : keypadDir,
-        );
-        newPads[i] = newPad;
-
-        if (i === pads.length - 1) {
-          newOut = action ?? "";
-        }
-      }
-
-      q.push([out + newOut, d + 1, ...newPads]);
-    });
-  }
-}
-
-function bfsPaths(start, path, keypad, n) {
-  const state = ["", "", ...Array(n).fill(start)];
-  const q = [];
-  q.push(state);
-
-  const seen = new Set();
-  while (q.length) {
-    let [out, d, ...pads] = q.shift();
-
-    if (out === path) {
-      return d;
-    }
-
-    if (
-      pads.some((pad) => !pad) ||
-      !path.startsWith(out) ||
-      seen.has([d, ...pads].toString())
-    ) {
+    if (curr === end) {
+      paths.add(d);
       continue;
     }
 
-    seen.add([d, ...pads].toString());
+    actions
+      .filter((a) => a !== "A")
+      .forEach((action) => {
+        const next = apply(action, curr, keypad);
 
-    actions.forEach((a) => {
-      let action = a;
-      let newPad = null;
-      let newOut = "";
-      const newPads = [...pads];
-
-      for (let i = 0; i < pads.length; i++) {
-        [newPad, action] = apply(action, pads[i], keypad);
-        newPads[i] = newPad;
-
-        if (i === pads.length - 1) {
-          newOut = action ?? "";
-        }
-      }
-
-      q.push([out + newOut, d + a, ...newPads]);
-    });
+        q.push([next, d + action]);
+      });
   }
+  return [...paths].map((p) => p + "A");
 }
 
 const dirkeys = keypadDir.flat().join("");
@@ -130,10 +67,10 @@ const dirpaths = new Map();
 
 for (let i = 0; i < dirkeys.length; i++) {
   for (let j = i; j < dirkeys.length; j++) {
-    const path = bfsPaths(dirkeys[i], dirkeys[j], keypadDir, 1);
+    const path = bfsPaths(dirkeys[i], dirkeys[j], keypadDir);
     dirpaths.set([dirkeys[i], dirkeys[j]].toString(), path);
 
-    const rpath = bfsPaths(dirkeys[j], dirkeys[i], keypadDir, 1);
+    const rpath = bfsPaths(dirkeys[j], dirkeys[i], keypadDir);
     dirpaths.set([dirkeys[j], dirkeys[i]].toString(), rpath);
   }
 }
@@ -151,53 +88,44 @@ for (let i = 0; i < numkeys.length; i++) {
   }
 }
 
-console.log(dirpaths);
-console.log(numpaths);
-
 function calcCosts(pads) {
   const padCosts = new Array(pads).fill(1).map(() => new Map());
-  [...dirpaths.entries()].forEach(([p, p1]) => padCosts[0].set(p, p1.length));
+  [...dirpaths.entries()].forEach(([p, p1]) =>
+    padCosts[0].set(
+      p,
+      chain(p1)
+        .minBy((x) => x.length)
+        .value().length,
+    ),
+  );
 
   for (let i = 1; i < padCosts.length; i++) {
     const paths = i === padCosts.length - 1 ? numpaths : dirpaths;
-    for (const [p, p1] of paths) {
-      let cost = 0;
-      for (let j = 0; j < p1.length; j++) {
-        cost += padCosts[i - 1].get([p1[j - 1] ?? "A", p1[j]].toString());
-      }
-      padCosts[i].set(p, cost);
+    for (const [p, path] of paths) {
+      path.forEach((p1) => {
+        let cost = 0;
+        for (let j = 0; j < p1.length; j++) {
+          cost += padCosts[i - 1].get([p1[j - 1] ?? "A", p1[j]].toString());
+        }
+        padCosts[i].set(p, Math.min(cost, padCosts[i].get(p) ?? Infinity));
+      });
     }
   }
 
   return padCosts;
 }
 
-function part1() {
-  return chain(codes)
-    .map((code) => bfs(code) * Number.parseInt(code))
-    .sum()
-    .value();
-}
-
-function part2() {
-  const pads = 3;
-  const costs = calcCosts(pads);
-
-  console.log(costs);
+function calcCodesCost(n) {
+  const costs = calcCosts(n);
 
   return chain(codes)
     .map((code) => {
-      console.log(code);
       const cost = chain(code)
-        .reduce((sum, _, i) => {
-          console.log(
-            [code[i - 1] ?? "A", code[i]].toString(),
-            costs[pads - 1].get([code[i - 1] ?? "A", code[i]].toString()),
-          );
-          return (
-            sum + costs[pads - 1].get([code[i - 1] ?? "A", code[i]].toString())
-          );
-        }, 0)
+        .reduce(
+          (sum, _, i) =>
+            sum + costs[n - 1].get([code[i - 1] ?? "A", code[i]].toString()),
+          0,
+        )
         .value();
       return cost * Number.parseInt(code);
     })
@@ -205,5 +133,13 @@ function part2() {
     .value();
 }
 
-// console.log(part1());
+function part1() {
+  return calcCodesCost(3);
+}
+
+function part2() {
+  return calcCodesCost(26);
+}
+
+console.log(part1());
 console.log(part2());
